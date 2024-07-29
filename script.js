7c7f64ecf83c434fb18951ad295fbfc6d8de83a4
Jordan Robinson <jordanr.com@gmail.com>
2024-07-29 15:21:38 -0400

    Just more updates

---
+++ script.js
@@ -0,0 +1,16 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+// let li = "";
+const buttonClick = document.getElementById("TaskAdd);
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("you must write something");
+    } 
+    else {
+        let li = doocument.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
+}
+buttonClick.addEventListener("click", addTask);
