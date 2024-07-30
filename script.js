fb9db88245b5050f29de43ca32c660a4426a95b2
Jordan Robinson <jordanr.com@gmail.com>
2024-07-29 20:04:29 -0400

    Trying to make inputBox.value empty - NOT Working

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 const inputBox = document.getElementById("input-box");
-
 const listContainer = document.getElementById("list-container");
-// let li = "";
-
 const buttonClick = document.getElementById("TaskAdd");
 
 function addTask(){
     if(inputBox.value === ""){
-        alert("you must write something");
+        alert("You must write something!");
     }
-    else {
-       let li = document.createElement("li");
+    else{
+        let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
     }
