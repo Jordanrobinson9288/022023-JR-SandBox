a7e7db632c36ac6da8843ed7332a0800fe7b9083
Jordan Robinson <jordanr.com@gmail.com>
2024-08-03 17:58:27 -0400

    new temp.html

--- practices.js
+++ practices.js
@@ -1,1 +1,15 @@
-Array
+/* 1. Write a JavaScript program to display the current day and time in the following format.  
+-------
+Sample Output : Today is : Tuesday.
+Current time is : 10 PM : 30 : 38 */
+
+const pday = document.getElementsByClassName("getDay");
+const phour = document.getElementsByClassName("getHour");
+const currentDay = Date.getDay();
+const getTheHour = Date.getHour(); 
+const getTheTime = Date.getTime();
+
+console.log(currentDay);
+
+pday.innerHTML === currentDay;
+phour.innerHTML === getTheHour;
